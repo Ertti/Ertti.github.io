5cd28e1e633c8e190d3a65c3881c6dda26c67276
Ertti <maaax1333@mail.ru>
2018-11-15 14:04:40 +0300

    resize
    resize function js

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+var customFrom = document.getElementsByClassName('customForm')[0],
+	customWidth = customFrom.offsetWidth;
+
+function customResize() {
+	if (customWidth >= 1599) {
+  		customFrom.classList.remove("customForm__md", "customForm__sm");
+		customFrom.classList.add("customForm__lg");
+	} else if (customWidth >= 640 && customWidth <= 1023) {
+		customFrom.classList.remove("customForm__lg", "customForm__sm");
+		customFrom.classList.add("customForm__md");
+	} else if (customWidth <= 639) {
+		customFrom.classList.remove("customForm__lg", "customForm__md");
+		customFrom.classList.add("customForm__sm");
+	} else {
+		customFrom.classList.remove("customForm__lg", "customForm__md", "customForm__sm");
+	}
+}
+customResize()
+
+window.addEventListener('resize', function(event){
+	customWidth = customFrom.offsetWidth;
+  	customResize()
+});
